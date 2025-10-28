dbbd7d76da60820b76b64d330796c53854ca91d2
Hiroyuki <kmc2539@kamiyama.ac.jp>
2025-10-28 14:40:26 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -2,8 +2,8 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/HiroyukiGPU/homebrew-dote"
   url "https://raw.githubusercontent.com/HiroyukiGPU/homebrew-dote/main/dote.c"
-  sha256 "2c2374198870d7f69dabb54f47d5f02a623d150a5eead34d4ed212b014ae906d"
-  version "1.0.1"
+  sha256 "32fcbcf3f14d1679440395ef86dd874fd3581866eabeb8725140b87dff4b7b0a"
+  version "1.0.2"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
