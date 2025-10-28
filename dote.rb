3e3e7e5c64870399fbbf9af89787bb0adf5601f2
Hiroyuki <kmc2539@kamiyama.ac.jp>
2025-10-28 11:06:03 +0900

    Add version 1.0.1 to dote.rb

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/HiroyukiGPU/homebrew-dote"
   url "https://raw.githubusercontent.com/HiroyukiGPU/homebrew-dote/main/dote.c"
   sha256 "2c2374198870d7f69dabb54f47d5f02a623d150a5eead34d4ed212b014ae906d"
+  version "1.0.1"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
